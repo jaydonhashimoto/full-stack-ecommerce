ac004382a8038471b5b213f111f9351a0c405b5f
jaydonhashimoto <jaydonhashi@gmail.com>
2019-07-03 18:16:30 -0700

    added bootstrap css

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
