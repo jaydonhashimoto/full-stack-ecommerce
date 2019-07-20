25d1d495167a3b1f0e351119645d263a22473f5a
jaydonhashimoto <jaydonhashi@gmail.com>
2019-07-20 00:07:07 -0700

    truncated title

--- client/src/components/EBookList.js
+++ client/src/components/EBookList.js
@@ -1,7 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import {
-    Container, Row, Col, Card, CardImg, CardBody,
-    CardTitle, CardSubtitle, Button
+  Container,
+  Row,
+  Col,
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -10,106 +17,142 @@ import PropTypes from 'prop-types';
 import UpdateEBookModal from '../components/UpdateEBookModal';
 
 export class EBookList extends Component {
-    state = {
-        ebook: {}
-    }
+  state = {
+    ebook: {}
+  };
 
-    static propTypes = {
-        getEBooks: PropTypes.func.isRequired,
-        deleteEBook: PropTypes.func.isRequired,
-        updateEBook: PropTypes.func.isRequired,
-        eBook: PropTypes.object.isRequired,
-        auth: PropTypes.object.isRequired
-    };
+  static propTypes = {
+    getEBooks: PropTypes.func.isRequired,
+    deleteEBook: PropTypes.func.isRequired,
+    updateEBook: PropTypes.func.isRequired,
+    eBook: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired
+  };
 
-    componentDidMount() {
-        this.props.getEBooks();
-    }
+  componentDidMount() {
+    this.props.getEBooks();
+  }
 
-    deleteEBook = (id, img) => {
-        this.props.deleteEBook(id, img);
-    }
+  deleteEBook = (id, img) => {
+    this.props.deleteEBook(id, img);
+  };
 
-    updateEBook = (ebook) => {
-        this.props.updateEBook(ebook);
-    }
+  updateEBook = ebook => {
+    this.props.updateEBook(ebook);
+  };
 
-    render() {
-        //get variable from initialState
-        const { isAuthenticated, user } = this.props.auth;
+  render() {
+    //get variable from initialState
+    const { isAuthenticated, user } = this.props.auth;
 
-        const { eBooks } = this.props.eBook;
+    const { eBooks } = this.props.eBook;
 
-        let imgSrc = '/images/';
-        return (
-            <div>
-                <Container>
-                    <Row>
-                        {eBooks.map(({ id, title, description, img, author, price, date_added, user_id }) => (
-                            <Col md="3" lg="3" sm="6" xs="6" key={id}>
-                                <Card style={cardStyle}>
-                                    <Link
-                                        style={linkStyle}
-                                        to={{
-                                            pathname: '/ebook',
-                                            search: `id=${id}`,
-                                            state: {
-                                                ebook: {
-                                                    id, title, description, img, author, price, date_added, user_id
-                                                }
-                                            }
-                                        }}
-                                    >
-                                        {!img || img.trim() !== 'noimage' ? (
-                                            <CardImg top width="100%" src={imgSrc + img} alt={title} />
-                                        ) : (
-                                                <CardImg top width="100%" src={imgSrc + 'notfound.png'} alt={title} />
-                                            )
-                                        }
-                                    </Link>
-                                    <CardBody>
-                                        <CardTitle><b>{title}</b></CardTitle>
-                                        <CardSubtitle><small>{author}</small></CardSubtitle>
-                                        {
-                                            isAuthenticated ? (
-                                                user.id === user_id ? (
-                                                    <Fragment>
-                                                        <Button color="danger" onClick={() => this.deleteEBook(id, img)}>Delete</Button>
-                                                        <UpdateEBookModal
-                                                            id={id}
-                                                            title={title}
-                                                            description={description}
-                                                            img={img}
-                                                            price={price}
-                                                            author={author}
-                                                        />
-                                                    </Fragment>
-                                                ) : (null)
-                                            ) : (null)
-                                        }
-                                    </CardBody>
-                                </Card><br />
-                            </Col>
-                        ))}
-                    </Row>
-                </Container>
-            </div>
-        )
-    }
+    let imgSrc = '/images/';
+    return (
+      <div>
+        <Container>
+          <Row>
+            {eBooks.map(
+              ({
+                id,
+                title,
+                description,
+                img,
+                author,
+                price,
+                date_added,
+                user_id
+              }) => (
+                <Col md="3" lg="3" sm="6" xs="6" key={id}>
+                  <Card>
+                    <Link
+                      style={linkStyle}
+                      to={{
+                        pathname: '/ebook',
+                        search: `id=${id}`,
+                        state: {
+                          ebook: {
+                            id,
+                            title,
+                            description,
+                            img,
+                            author,
+                            price,
+                            date_added,
+                            user_id
+                          }
+                        }
+                      }}
+                    >
+                      {!img || img.trim() !== 'noimage' ? (
+                        <CardImg
+                          top
+                          width="100%"
+                          src={imgSrc + img}
+                          alt={title}
+                        />
+                      ) : (
+                        <CardImg
+                          top
+                          width="100%"
+                          src={imgSrc + 'notfound.png'}
+                          alt={title}
+                        />
+                      )}
+                    </Link>
+                    <CardBody>
+                      <CardTitle>
+                        <b>
+                          <div className="home-title">{title}</div>
+                        </b>
+                      </CardTitle>
+                      <CardSubtitle>
+                        <small>{author}</small>
+                      </CardSubtitle>
+                      {isAuthenticated ? (
+                        user.id === user_id ? (
+                          <Fragment>
+                            <Button
+                              color="danger"
+                              onClick={() => this.deleteEBook(id, img)}
+                            >
+                              Delete
+                            </Button>
+                            <UpdateEBookModal
+                              id={id}
+                              title={title}
+                              description={description}
+                              img={img}
+                              price={price}
+                              author={author}
+                            />
+                          </Fragment>
+                        ) : null
+                      ) : null}
+                    </CardBody>
+                  </Card>
+                  <br />
+                </Col>
+              )
+            )}
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 const linkStyle = {
-    textDecoration: 'none',
-    color: 'black'
-}
+  textDecoration: 'none',
+  color: 'black'
+};
 
-const cardStyle = {
-    // height: '100%'
-}
-
-const mapStateToProps = (state) => ({
-    eBook: state.eBook,
-    auth: state.auth
+const mapStateToProps = state => ({
+  eBook: state.eBook,
+  auth: state.auth
 });
 
-export default connect(mapStateToProps, { getEBooks, deleteEBook, updateEBook })(EBookList);
+export default connect(
+  mapStateToProps,
+  { getEBooks, deleteEBook, updateEBook }
+)(EBookList);
